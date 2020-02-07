cdc25871920c5bc0497ba60defee0a23fe6e0063
Mishel Agüero <m.aguero@outlook.com>
2020-02-07 12:57:00 -0600

    Adding logic to solve this problem

--- src/main/java/com/algoritm/SecondHigher.java
+++ src/main/java/com/algoritm/SecondHigher.java
@@ -2,6 +2,18 @@ package com.algoritm;
 
 public class SecondHigher {
     public static int find(int[] unsortedArray) {
-        return 0;
+        int firstNumber = unsortedArray[0];
+        int secondNumber = unsortedArray[0];
+
+        for(int i = 1; i < unsortedArray.length; i++) {
+            if(firstNumber < unsortedArray[i]) {
+                secondNumber = firstNumber;
+                firstNumber = unsortedArray[i];
+            } else if (secondNumber < unsortedArray[i] || firstNumber == secondNumber) {
+                secondNumber = unsortedArray[i];
+            }
+        }
+
+        return secondNumber;
     }
 }
